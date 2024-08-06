8728c56bc9a4d24fa1f0164f71b6bb7bfb35fd7b
BrunoLeblein2 <brunoleblein@gmail.com>
2024-08-06 19:53:34 -0300

    Arrumando imagens e textos do filtro

--- src/components/Filtro.tsx
+++ src/components/Filtro.tsx
@@ -7,34 +7,34 @@ export function Filtro() {
         <div className="flex gap-x-10 justify-center">
             <div className="h-[685px] w-[1183px]">
                 <div className="flex flex-col items-center text-center">
-                    <h1 className='text-2xl font-Poppins-ExtraBold'>
+                    <h1 className='text-3xl font-Poppins-ExtraBold text-[#333333] font-bold'>
                         Browse The Range
                     </h1>
-                    <p className='text-sm font-Poppins'>
+                    <p className='text-xl font-Poppins text-[#666666] font-normal'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     </p>
                 </div>
-                <div className="flex justify-around mt-8">
+                <div className="flex justify-around mt-8 text-[#333333] font-semibold">
                     <div className="flex flex-col items-center">
-                        <img src={dining} alt="Dining"/>
+                        <img className='h-[480px] w-[381px] rounded-lg' src={dining} alt="Dining"/>
                         <div className="p-4">
-                            <h2 className="text-base font-semibold font-Poppins">
+                            <h2 className="text-2xl	 font-semibold font-Poppins">
                                 Dining
                             </h2>
                         </div>
                     </div>
                     <div className="flex flex-col items-center">
-                        <img src={living} alt="Living"/>
+                        <img className='h-[480px] w-[381px] rounded-lg' src={living} alt="Living"/>
                         <div className="p-4">
-                            <h2 className="text-base font-semibold font-Poppins">
+                            <h2 className="text-2xl font-semibold font-Poppins">
                                 Living
                             </h2>
                         </div>
                     </div>
                     <div className="flex flex-col items-center">
-                        <img src={bedroom} alt="Bedroom"/>
+                        <img className='h-[480px] w-[381px] rounded-lg' src={bedroom} alt="Bedroom"/>
                         <div className="p-4">
-                            <h2 className="text-base font-semibold font-Poppins">
+                            <h2 className="text-2xl font-semibold font-Poppins">
                                 Bedroom
                             </h2>
                         </div>
