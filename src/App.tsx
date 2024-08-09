4311e31e690d08d0e0f611f206edc23cb9d06ab5
enzoguis <enzog0315@gmail.com>
2024-08-08 21:04:59 -0300

    finalizando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import { BannerInicio } from './components/BannerInicio'
-import { Filtro } from './components/Filtro'
-import { Cards } from './components/Cards'
-import { CardsDown } from './components/CardsDown'
-import { Carrossel } from './components/Carrossel'
-import { Button } from './components/Button'
-import { ImagemBaixo } from './components/ImagemBaixo'
-
+import Home from "./pages/Home";
 
 export default function App() {
   return (
-        <div className="flex flex-col items-center justify-center gap-y-16">
-          <BannerInicio />
-          <Filtro />
-          <div className='flex flex-col items-center justify-center gap-y-8'>
-            <p className='font-bold text-[50px] text-[#3A3A3A]'>Our Products</p>
-            <Cards />
-            <CardsDown />
-          </div>
-            <Button />
-            <ImagemBaixo />
-        </div>
+    <Home />
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import { BannerInicio } from '../components/BannerInicio'
+import { Filtro } from '../components/Filtro'
+import { Cards } from '../components/Cards'
+import { CardsDown } from '../components/CardsDown'
+import { Carrossel } from '../components/Carrossel'
+import { Button } from '../components/Button'
+import { ImagemBaixo } from '../components/ImagemBaixo'
+
+
+export default function Home() {
+  return (
+        <div className="flex flex-col items-center justify-center gap-y-16">
+          <BannerInicio />
+          <Filtro />
+          <div className='flex flex-col items-center justify-center gap-y-8'>
+            <p className='font-bold text-[50px] text-[#3A3A3A]'>Our Products</p>
+            <Cards />
+            <CardsDown />
+          </div>
+            <Button />
+            <Carrossel />
+            <ImagemBaixo />
+        </div>
+  )
+}
